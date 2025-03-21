64c238590918165fe797ef3569fb36c95f651bf0
Anthony Sergeevich <jespy.kz@gmail.com>
2025-03-21 10:09:16 +0530

    Добавлен middleware для определения прав персонала

--- src/middlewares.py
+++ src/middlewares.py
@@ -47,10 +47,50 @@ class AuthMiddleware(BaseMiddleware):
 
         try:
             api = APIManager()
-            data["has_access"] = await api.check_telegram_id(
+            data["is_authenticated"] = await api.check_telegram_id(
                 str(message.from_user.id)
             )
         except exc.APIError:
-            data["has_access"] = False
+            data["is_authenticated"] = False
+
+        return await handler(event, data)
+
+
+class StaffMiddleware(BaseMiddleware):
+    """
+    Middleware для проверки доступа персонала.
+    """
+
+    async def __call__(
+        self,
+        handler: Callable[["TelegramObject", Dict[str, Any]], Awaitable[Any]],
+        event: "TelegramObject",
+        data: Dict[str, Any],
+    ) -> Any:
+        if isinstance(event, Message):
+            message = event
+        elif isinstance(event, CallbackQuery):
+            if event.message is None:
+                raise ValueError("CallbackQuery does not contain a message")
+            if isinstance(event.message, InaccessibleMessage):
+                raise ValueError(
+                    "CallbackQuery contains an inaccessible message"
+                )
+            message = event.message
+        else:
+            raise TypeError("Non authorized event")
+
+        if message.from_user is None:
+            raise ValueError(
+                "Message does not have a sender (from_user is None)"
+            )
+
+        try:
+            api = APIManager()
+            data["is_staff"] = await api.check_is_staff(
+                str(message.from_user.id)
+            )
+        except exc.APIError:
+            data["is_staff"] = False
 
         return await handler(event, data)
