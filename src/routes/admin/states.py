51509ea4628a65738c8f4dbfae602e6a8c6bef35
Anthony Sergeevich <jespy.kz@gmail.com>
2025-03-27 14:57:06 +0530

    Написаны маршруты для /admin

--- src/routes/admin/states.py
+++ src/routes/admin/states.py
@@ -14,3 +14,38 @@ class NewSideBookingState(StatesGroup):
     bikes = State()
     amount = State()
     confirm = State()
+
+
+class AcceptBookingAdminState(StatesGroup):
+    """
+    Состояния для принятия проката администратором.
+    """
+
+    booking_id = State()
+    instructor = State()
+    bike = State()
+
+
+class CancelBookingAdminState(StatesGroup):
+    """
+    Состояния для отмены проката администратором.
+    """
+
+    booking_id = State()
+
+
+class PendingBookingAdminState(StatesGroup):
+    """
+    Состояния для перевода проката в ожидание администратором.
+    """
+
+    booking_id = State()
+
+
+class AdminChangeInstructorState(StatesGroup):
+    """
+    Состояния замены инструктора администратором.
+    """
+
+    booking_id = State()
+    instructor = State()
